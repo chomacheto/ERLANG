eabd080241536622a91311e69fd6d649d7aaa671
chomacheto <68460255+chomacheto@users.noreply.github.com>
2022-03-06 12:39:20 +0200

    Initial commit

---
+++ tut.erl
@@ -0,0 +1,10 @@
+-module(tut).
+-import(string, [len/1, concat/2, chr/2, substr/3, str/2, to_lower/1, to_upper/1]).
+
+-export([main/0]).
+
+main() ->
+    do_math(5, 4).
+
+do_math(A, B) ->
+    math:log10(1000).
